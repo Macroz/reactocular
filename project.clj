468890769e26abf4c6fa7cd5ec676944cabb0189
Markku Rontu <markku.rontu@iki.fi>
2015-10-14 23:09:56 +0300

    Bump version to 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject macroz/reactocular "0.1.0"
+(defproject macroz/reactocular "0.1.1"
   :description "Visual brutish component scan of React projects"
   :url "https://github.com/Macroz/reactocular"
   :license {:name "Eclipse Public License"
