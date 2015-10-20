048d531bfc2a9ce2812fb23f19a2600f46160961
Markku Rontu <markku.rontu@iki.fi>
2015-10-20 08:39:55 +0300

    Add dependency to medley

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [macroz/tangle "0.1.9"]]
+                 [macroz/tangle "0.1.9"]
+                 [medley "0.7.0"]]
   :main reactocular.core
   :aot :all
 )
